c18559e6700bdba527f3ee5ad5bfbee4657d8f68
Adelle Pitsas <apitsas@gmri.org>
2024-08-30 12:12:53 -0400

    add info about file-based naming to tutorial

--- tutorial/pages/1_Additional_Pages.py
+++ tutorial/pages/1_Additional_Pages.py
@@ -8,12 +8,15 @@ st.code('''
         main_directory/
             application.py
             pages/
-                page1.py
-                page2.py
-                page3.py
+                1_Example_Page_One.py
+                2_Example_Page_Two.py
+                3_Example_Page_Three.py
         ''')
+st.write("Additional page titles will displayed in the sidebar based on what the file name is.")
 
-st.write('Code example for `Additional_Pages.py`:')
+st.write("----")
+
+st.write('This is what this page (`Additional Pages`) looks in code:')
 st.code('''
         import streamlit as st
 
