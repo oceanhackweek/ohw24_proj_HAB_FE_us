e074f9f78478e763eb46ed43544d69aff38399e7
Adelle Pitsas <apitsas@gmri.org>
2024-08-30 11:43:55 -0400

    make text rainbow

--- app/app.py
+++ app/app.py
@@ -48,7 +48,23 @@ if len(selected_dates) == 1:
 # raise error if dates are more than 5 days apart
 elif abs((selected_dates[1] - selected_dates[0]).days) > 5:
     raise ValueError("Please select a date range of 5 days or less")
-st.markdown("<h1 style='text-align: center;'>3D Hyperspectral Data</h1>", unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .gradient-text {
+        background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
+        -webkit-background-clip: text;
+        color: transparent;
+        text-align: center;
+        font-size: 2em;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<h1 class="gradient-text">3D Hyperspectral Data</h1>', unsafe_allow_html=True)
+
 # dask setup
 # run the following in terminal before spinning up the app
 # coiled login --token 48c0361f39984d7b8bab62f3252a5d7e-e6c38c5e8b15f967b104cf58ee289f4066ccd330
@@ -204,4 +220,4 @@ with col1b:
 #     # Process and visualize the chlorophyll-a data
 #     fig1 = process_and_viz_chl(results_chl)
 
-#     st.pyplot(fig1) 
+#     st.pyplot(fig1)
